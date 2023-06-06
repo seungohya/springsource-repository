7c0d44703638221cbf1e00f9fcce330d7b56fd4d
seungoh <seungoh6@gmail.com>
2023-06-07 02:10:45 +0900

    security

---
+++ board_webmvc_security/src/main/java/com/spring/controller/MemberController.java
@@ -0,0 +1,17 @@
+package com.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+@RequestMapping("/member")
+public class MemberController {
+	@GetMapping("/login")
+	public void loginGet() {
+		log.info("로그인 폼 요청");
+	}
+}
